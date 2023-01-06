6ddd4507e90bcf9e9eae7200438d08f5bb5bfcd4
“Vicky” <vicky.midson@opencastsoftware.com>
2023-01-06 14:42:33 +0000

    Eleventy url stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,19 @@
 module.exports = function (eleventyConfig) {
   // Options to customise the appearance of your design history
+  const url = process.env.GITHUB_ACTIONS
+  ? 'https://vickymidson.github.io/design-history/'
+  : '/'
+const pathPrefix = process.env.GITHUB_ACTIONS
+  ? '/design-history/'
+  : '/'
   // https://x-govuk.github.io/govuk-eleventy-plugin/options/
   eleventyConfig.addPlugin(require('govuk-eleventy-plugin'), {
     stylesheets: [
       '/styles/application.css'
     ],
     headingPermalinks: true,
+    pathPrefix, 
+    url,
     header: {
       organisationLogo: false,
       productName: 'Apply for a juggling licence design history',
